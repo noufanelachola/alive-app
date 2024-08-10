e565aabb2a93f35a871f9451eb1de0c590c5ef5f
noufanelachola <noufansadiqelachola@gmail.com>
2024-08-10 11:31:32 +0530

    completed the register and logIn

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,13 +1,14 @@
 import "./Register.css";
 
-function Register(){
+function Register({changeRoute}){
 
     return(
         <section id="register">
             <input className="inputName" placeholder="Enter your name" />
             <input className="inputUserName" placeholder="Enter your username" />
             <input className="inputPassword" placeholder="Enter your password" type="password" />
-            <p className="toggleText font-md color-secondary-blue btn">LogIn</p>
+            <button className="btn inBtn color-white font-weight-md">Register</button>
+            <p className="toggleText font-md color-secondary-blue btn" onClick={()=>changeRoute("logIn")} >LogIn</p>
         </section>
     );
 
